d45163cbac35c59e3cac9bbbda7cbf31a16cc887
tannaz119 <katy.kitcat@gmail.com>
2026-06-05 16:04:20 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "dental-os-v1";
+
+self.addEventListener("install", e => {
+e.waitUntil(
+caches.open(CACHE).then(cache => {
+return cache.addAll([
+"/",
+"/index.html",
+"/style.css",
+"/app.js"
+]);
+})
+);
+});
+
+self.addEventListener("fetch", e => {
+e.respondWith(
+caches.match(e.request).then(res => {
+return res || fetch(e.request);
+})
+);
+});
